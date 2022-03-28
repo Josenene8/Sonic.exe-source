7cbb12e3e341bbc4b3f4e4835380e169ad81ff85
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-28 20:15:48 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -215,8 +215,8 @@ class StoryMenuState extends MusicBeatState
                 new FlxTimer().start(1, function(tmr:FlxTimer)
                 {
                     //LoadingState.loadAndSwitchState(new PlayState(), true); //save this code for the cutsceneless build of the game
-                    var video:MP4Handler = new MP4Handler();
-                    video.playMP4(Paths.video('tooslowcutscene1'), new PlayState()); 
+                   
+                    
                 });
                 
             }
